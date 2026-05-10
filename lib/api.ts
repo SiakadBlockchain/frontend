7a8aa589db057da99a794dacfd64e4e00ad08f61
ShotakanLe <ramenikoze0909@gmail.com>
2026-05-11 01:51:09 +0700

    setup vm

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API;
 
 const api = axios.create({
   baseURL: `${BASE_URL}/siakadBlockchain/api`,
